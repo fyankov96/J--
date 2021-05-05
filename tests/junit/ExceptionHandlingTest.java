c7b00429a8b0b78fc48927ecc0bf0091a91bc3f8
Dario Passarello <dario.passarello@gmail.com>
2021-05-05 16:20:06 +0200

    test: revised some tests

--- tests/junit/ExceptionHandlingTest.java
+++ tests/junit/ExceptionHandlingTest.java
@@ -17,5 +17,14 @@ public class ExceptionHandlingTest extends TestCase {
 
     public void testDivide() {
         this.assertEquals(eh.tryCatch(), 0);
-    }    
+
+    }
+    
+    public void testExceptions() {
+        System.out.println("Running Exception Tests...");
+        this.assertEquals("Division by Zero should be triggered and caught", eh.divideByZero(), "Caught");
+        this.assertEquals("Illegal Array access should be caught", eh.illegalAccess(), "Caught");
+        this.assertEquals("Illegal Argument should not be thrown", eh.IllegalArgument("Illegal"), "Caught");
+        this.assertEquals("Illegal Argument should be thrown", eh.IllegalArgument("Legal"), "Caught");
+    }
 }
--- tests/pass/OrAssign.java
+++ tests/pass/OrAssign.java
@@ -1,8 +1,7 @@
 package pass;
 
 public class OrAssign {
-    public boolean orAssign(boolean a){
-        boolean b = false;
+    public boolean orAssign(int a, int b){
         b |= a;
         return b;
     }    
